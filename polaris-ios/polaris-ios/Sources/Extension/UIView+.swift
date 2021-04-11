0556f860b27eaf7d757dc85ed0903dc2313d2fc6
dongminyoon <ehdals8965@naver.com>
2021-04-12 01:22:46 +0900

    setup: UIViewController+Extension, StoryboardName 추가

--- polaris-ios/polaris-ios/Sources/Extension/UIView+.swift
+++ polaris-ios/polaris-ios/Sources/Extension/UIView+.swift
@@ -12,5 +12,11 @@ extension UIView {
         let nibName = String(describing: self)
         return Bundle.main.path(forResource: nibName, ofType: "nib") != nil
     }
+    
+    static func fromNib<T: UIView>() -> T? {
+        let identifier = String(describing: T.self)
+        guard let nib = Bundle.main.loadNibNamed(identifier, owner: nil, options: nil) else { return nil }
+        return nib.first as? T
+    }
 }
 
