2a78f28577c694928b66d74fa8abd25c85df4862
dongmin.yoon <dongmin.yoon@naverz-corp.com>
2021-04-17 13:04:06 +0900

    feat: Constants 폴더 - UserDefaultKey, StoryboardName 이동

---
+++ polaris-ios/polaris-ios/Sources/Constants/UserDefaultKey.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKeys.swift
+//  polaris-ios
+//
+//  Created by USER on 2021/04/15.
+//
+
+import Foundation
+
+struct UserDefaultsKey {
+    static let auth = "authrorication"
+}
