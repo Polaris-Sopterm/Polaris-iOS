f738b59542bb7b6ed00cc2e7001b306d7c259e3a
dongmin.yoon <dongmin.yoon@naverz-corp.com>
2021-04-17 13:05:21 +0900

    feat: UIColor+ 추가

--- polaris-ios/polaris-ios/Sources/Extension/UIColor+.swift
+++ polaris-ios/polaris-ios/Sources/Extension/UIColor+.swift
@@ -36,3 +36,49 @@ extension UIColor {
                   alpha: alpha)
     }
 }
+
+extension UIColor {
+    @nonobjc class var maintext: UIColor {
+        return UIColor(red: 64.0 / 255.0, green: 64.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainPurple: UIColor {
+        return UIColor(red: 105.0 / 255.0, green: 105.0 / 255.0, blue: 192.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var inactiveBtn: UIColor {
+        return UIColor(red: 240.0 / 255.0, green: 243.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var field: UIColor {
+        return UIColor(white: 250.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var inactiveSky: UIColor {
+        return UIColor(red: 229.0 / 255.0, green: 248.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var inactiveTextSky: UIColor {
+        return UIColor(red: 185.0 / 255.0, green: 231.0 / 255.0, blue: 241.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainSky: UIColor {
+        return UIColor(red: 127.0 / 255.0, green: 219.0 / 255.0, blue: 239.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var inactivePurple: UIColor {
+        return UIColor(red: 233.0 / 255.0, green: 233.0 / 255.0, blue: 246.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var inactiveText: UIColor {
+        return UIColor(red: 203.0 / 255.0, green: 212.0 / 255.0, blue: 212.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var textSky: UIColor {
+        return UIColor(red: 104.0 / 255.0, green: 199.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var inactiveTextPurple: UIColor {
+        return UIColor(red: 198.0 / 255.0, green: 198.0 / 255.0, blue: 221.0 / 255.0, alpha: 1.0)
+    }
+}
