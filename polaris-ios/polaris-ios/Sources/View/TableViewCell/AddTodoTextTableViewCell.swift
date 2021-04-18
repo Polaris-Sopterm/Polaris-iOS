ed8d082cb7103a21687c42b5e16548041cce54e1
dongmin.yoon <dongmin.yoon@naverz-corp.com>
2021-04-18 14:14:12 +0900

    feat: AddTodoTextTableViewCell UI 구현

--- polaris-ios/polaris-ios/Sources/View/TableViewCell/AddTodoTextTableViewCell.swift
+++ polaris-ios/polaris-ios/Sources/View/TableViewCell/AddTodoTextTableViewCell.swift
@@ -10,19 +10,28 @@ import RxSwift
 import RxCocoa
 
 class AddTodoTextTableViewCell: AddTodoTableViewCell {
-    class override var cellHeight: CGFloat { return 100 }
+    class override var cellHeight: CGFloat {
+        let textFieldHeight: CGFloat = (screenRaito * 53)
+        let space: CGFloat           = 15
+        return (verticalInset * 2) + space + textFieldHeight
+    }
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textFieldContainerView: UIView!
+    var polarisMarginTextFieldView: PolarisMarginTextField? = UIView.fromNib()
     
     // MARK: - Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.setupTextFieldView()
     }
     
-    
-    // MARK: - Bind
-    private func bindTextField() {
-        
+    private func setupTextFieldView() {
+        guard let polarisMarginTextFieldView = self.polarisMarginTextFieldView else { return }
+        self.textFieldContainerView.addSubview(polarisMarginTextFieldView)
     }
+    
+    static let screenRaito: CGFloat         = DeviceInfo.screenWidth / 375
+    static let verticalInset: CGFloat       = 10
 }
