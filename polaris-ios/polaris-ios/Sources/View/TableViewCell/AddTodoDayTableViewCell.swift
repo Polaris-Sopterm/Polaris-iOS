05ec95e56ab942b1027d546c7018080771fd00ec
dongmin.yoon <dongmin.yoon@naverz-corp.com>
2021-04-22 00:28:41 +0900

    feat: AddTodoDayTableViewCell Layout 설정

--- polaris-ios/polaris-ios/Sources/View/TableViewCell/AddTodoDayTableViewCell.swift
+++ polaris-ios/polaris-ios/Sources/View/TableViewCell/AddTodoDayTableViewCell.swift
@@ -6,32 +6,42 @@
 //
 
 import UIKit
+import RxSwift
 
 class AddTodoDayTableViewCell: AddTodoTableViewCell {
     override class var cellHeight: CGFloat {
-        let verticalInset: CGFloat  = 10
-        let labelHeight: CGFloat    = 17
-        let spacing: CGFloat        = 15
-        
-        return 100
+        let labelHeight: CGFloat = 17
+        let spacing: CGFloat     = 15
+        return (verticalInset * 2) + dayCellHeight + spacing + labelHeight
     }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.layoutColletionView()
     }
     
     // MARK: - Set Up
     private func layoutColletionView() {
-        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize                 = CGSize(width:  type(of: self).dayCellWidth,
+                                                     height: type(of: self).dayCellHeight)
+            layout.sectionInset             = .zero
+            layout.minimumLineSpacing       = 2
+            layout.minimumInteritemSpacing  = 0
+        }
     }
-    
     
     
     private static let horizontalInset: CGFloat     = 23
     private static let verticalInset: CGFloat       = 10
     private static let screenRatio: CGFloat         = DeviceInfo.screenWidth / 375
-    private static let dayCellWidth: CGFloat        = 45 * screenRatio
     private static let dayCellHeight: CGFloat       = 66 * screenRatio
+    private static let dayCellWidth: CGFloat        = (DeviceInfo.screenWidth - (2 * horizontalInset) - (6 * 2)) / 7
+    
+    var viewModel  = AddTodoDayViewModel()
+    var disposeBag = DisposeBag()
 }
---
+++ polaris-ios/polaris-ios/Sources/ViewModel/AddTodoDayViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddTodoDayViewModel.swift
+//  polaris-ios
+//
+//  Created by USER on 2021/04/22.
+//
+
+import Foundation
+
+class AddTodoDayViewModel {
+    
+}
