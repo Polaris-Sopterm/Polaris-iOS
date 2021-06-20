15f4cce7ab53842607ef328243c539ac7b96e216
dongminyoon <ehdals8965@naver.com>
2021-06-21 01:13:29 +0900

    feat: PolarisPopupView 추가 - 확인, 취소 버튼이 있는 팝업

---
+++ polaris-ios/polaris-ios/Sources/View/CustomView/PolarisPopupView.swift
@@ -0,0 +1,79 @@
+//
+//  PolarisPopupView.swift
+//  polaris-ios
+//
+//  Created by Dongmin on 2021/06/21.
+//
+
+import RxCocoa
+import RxSwift
+import UIKit
+
+class PolarisPopupView: UIView {
+    
+    typealias Handler = (() -> Void)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.bindButtons()
+    }
+    
+    func configure(title: String, subTitle: String,
+                   cancelTitle: String = "취소", confirmTitle: String = "확인",
+                   confirmHandler: Handler? = nil, cancelHandler: Handler? = nil) {
+        self.titleLabel.text = title
+        self.subTitleLabel.text = subTitle
+        self.confirmButton.setTitle(confirmTitle, for: .normal)
+        self.cancelButton.setTitle(cancelTitle, for: .normal)
+        
+        self.confirmHandler = confirmHandler
+        self.cancelHandler  = cancelHandler
+    }
+    
+    func show(in view: UIView) {
+        view.addSubview(self)
+        self.snp.makeConstraints { make in make.edges.equalToSuperview() }
+    }
+    
+    private func animateForShow() {
+        self.alpha = 0
+        UIView.animate(withDuration: 0.2) {
+            self.alpha = 1
+        }
+    }
+    
+    private func animateForDismiss(completion: Handler? = nil) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.alpha = 0
+        }) { _ in
+            self.removeFromSuperview()
+            completion?()
+        }
+    }
+    
+    private func bindButtons() {
+        self.confirmButton.rx.tap
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.animateForDismiss(completion: self?.confirmHandler)
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.cancelButton.rx.tap
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.animateForDismiss(completion: self?.cancelHandler)
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    private let disposeBag = DisposeBag()
+    private var cancelHandler: Handler?
+    private var confirmHandler: Handler?
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subTitleLabel: UILabel!
+    @IBOutlet private weak var cancelButton: UIButton!
+    @IBOutlet private weak var confirmButton: UIButton!
+    
+}
