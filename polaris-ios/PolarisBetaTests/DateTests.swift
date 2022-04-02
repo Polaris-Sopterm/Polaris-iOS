8a7d3122fc7471c1268a84c2850663080fb0292a
qodhrkawk <gabriel1120@naver.com>
2022-04-02 12:35:42 +0900

    date 테스트 추가

---
+++ polaris-ios/PolarisBetaTests/DateTests.swift
@@ -0,0 +1,98 @@
+//
+//  DateTests.swift
+//  PolarisBetaTests
+//
+//  Created by Yunjae Kim on 2022/04/02.
+//
+
+import XCTest
+import Foundation
+//@testable import Polaris
+
+fileprivate extension Date {
+    
+    static func normalizedDate(date: Date) -> Date {
+        Calendar.koreaISO8601.date(bySettingHour: 12, minute: 0, second: 0, of: date) ?? date
+    }
+    
+    static func datesIncludedInWeekFromDate(date: Date) -> [Date] {
+        let weekDay = Calendar.koreaISO8601.component(.weekday, from: normalizedDate(date: date))
+        var datesIncludedWeek: [Date]      = []
+        var distancesBetweenWeekDay: [Int] = []
+        
+        if weekDay == WeekDay.sunday.rawValue {
+            distancesBetweenWeekDay = [-6, -5, -4, -3, -2, -1, 0] }
+        else {
+            for weekDayValue in WeekDay.monday.rawValue...WeekDay.saturday.rawValue {
+                let tempDistanceBetweenWeekDay = weekDayValue - weekDay
+                distancesBetweenWeekDay.append(tempDistanceBetweenWeekDay)
+            }
+            
+            let sundayWeekDay = 8
+            distancesBetweenWeekDay.append(sundayWeekDay - weekDay)
+        }
+        
+        distancesBetweenWeekDay.forEach { distance in
+            guard let calculatedDate = Calendar.koreaISO8601.date(byAdding: .day, value: distance, to: normalizedDate(date: date)) else { return }
+            datesIncludedWeek.append(calculatedDate)
+        }
+        
+        return datesIncludedWeek
+    }
+    
+    static func thursdayOfWeekIncludesDate(date: Date) -> Date {
+        return datesIncludedInWeekFromDate(date: normalizedDate(date: date))[3]
+    }
+    
+    static func weekNoOfDate(date: Date) -> Int {
+        Calendar.koreaISO8601.component(.weekOfMonth, from: Date.thursdayOfWeekIncludesDate(date: date))
+    }
+    
+    static func monthOfDate(date: Date) -> Int {
+        Calendar.koreaISO8601.component(.month, from: Date.thursdayOfWeekIncludesDate(date: date))
+    }
+    
+    static func yearOfDate(date: Date) -> Int {
+        Calendar.koreaISO8601.component(.year, from: Date.thursdayOfWeekIncludesDate(date: date))
+    }
+    
+    static func polarisDateOfDate(date: Date) -> PolarisDate {
+        PolarisDate(year: yearOfDate(date: date), month: monthOfDate(date: date), weekNo: weekNoOfDate(date: date))
+    }
+}
+
+class DateTests: XCTestCase {
+    
+    func testWhenThursDayIsLastDayOfMonthThenFridayReturnsSameWeekInfo() throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let thursDay = try XCTUnwrap(dateFormatter.date(from: "2022-03-31"))
+        let friday = try XCTUnwrap(dateFormatter.date(from: "2022-04-01"))
+        
+        XCTAssertEqual(Date.polarisDateOfDate(date: thursDay), Date.polarisDateOfDate(date: friday))
+        XCTAssertTrue(Date.polarisDateOfDate(date: thursDay).month == 3)
+    }
+    
+    func testWhenLastDayOfMonthIsMondayThenReturnsNextMonthInfo() throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let monday = try XCTUnwrap(dateFormatter.date(from: "2022-02-28"))
+        let tuesday = try XCTUnwrap(dateFormatter.date(from: "2022-03-01"))
+        
+        XCTAssertEqual(Date.polarisDateOfDate(date: monday), Date.polarisDateOfDate(date: tuesday))
+        XCTAssertTrue(Date.polarisDateOfDate(date: monday).month == 3)
+    }
+    
+    func testAlldayOfYearDoesNotReturnsZeroWeekNo() throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let firstDate = try XCTUnwrap(dateFormatter.date(from: "2022-01-01"))
+        
+        for distance in 0...365 {
+            guard let calculatedDate = Calendar.koreaISO8601.date(byAdding: .day, value: distance, to: firstDate) else { return }
+            let polarisDate = Date.polarisDateOfDate(date: calculatedDate)
+            XCTAssertNotEqual(polarisDate.weekNo, 0)
+        }
+    }
+}
